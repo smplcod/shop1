df632ed9dc504f428b529c9c187a82464e3128b1
smplcod <mplcod2021@gmail.com>
2022-08-11 19:16:16 +0600

    Added firebase read

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useState, useEffect } from "react";
 import Navigation from "./Navigation";
+import { db } from "./FirebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
 
 function App() {
-  return <Navigation />;
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getUsers();
+  }, []);
+  // return <Navigation />;
+  return (
+    <div className="App">
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <h2>Name: {user.name}</h2>
+            <h3>Age: {user.age}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/FirebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2VJX8an34O6ib252r5lCxkDSe71PDrss",
+  authDomain: "smplcod-shop-1.firebaseapp.com",
+  projectId: "smplcod-shop-1",
+  storageBucket: "smplcod-shop-1.appspot.com",
+  messagingSenderId: "809316262376",
+  appId: "1:809316262376:web:8497f3eeacc8746b5b445d",
+  measurementId: "G-LNGJZQR2ZQ",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
