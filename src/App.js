d83e85bd1890648168c7eab4eae43f19a638901d
smplcod <mplcod2021@gmail.com>
2022-08-11 22:03:42 +0600

    add crud create

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { useState, useEffect } from "react";
 import Navigation from "./Navigation";
 import { db } from "./FirebaseConfig";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
+import { Button, TextField } from "@mui/material";
 
 function App() {
+  const [newName, setNewName] = useState("");
+  const [newAge, setNewAge] = useState(0);
+
   const [users, setUsers] = useState([]);
   const usersCollectionRef = collection(db, "users");
+
+  const createUser = async () => {
+    await addDoc(usersCollectionRef, { name: newName, age: newAge });
+  };
+
   useEffect(() => {
     const getUsers = async () => {
       const data = await getDocs(usersCollectionRef);
@@ -13,9 +22,29 @@ function App() {
     };
     getUsers();
   }, []);
+
   // return <Navigation />;
+
   return (
     <div className="App">
+      <TextField
+        onChange={(e) => {
+          setNewName(e.target.value);
+        }}
+        placeholder="Name..."
+        variant="standard"
+      />
+      <TextField
+        onChange={(e) => {
+          setNewAge(e.target.value);
+        }}
+        type="number"
+        placeholder="Age..."
+        variant="standard"
+      />
+      <Button onClick={createUser} variant="outlined">
+        Create User
+      </Button>
       {users.map((user) => {
         return (
           <div key={user.id}>
